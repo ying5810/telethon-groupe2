e60665677cf37c8a6622d1fe23002114b9283404
David LEGRAND <wawawaformation@gmail.com>
2024-08-27 13:52:51 +0200

    initialisation du projet

---
+++ README.md
@@ -0,0 +1,10 @@
+# Telethon Groupe 2
+
+Fil rouge Téthon par petit groupe
+
+## participants
+- Émilia
+- Ying
+- Stanislas
+- Alain
+- Stéphan
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php $page_title = 'Accueil' ?>
+
+<?php  require 'include/head.php'; ?>
+
+<main>
+    Le conentu de la page
+
+</main>
+
+<?php  require 'include/foot.php'; ?>
