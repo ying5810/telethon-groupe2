06d659db98c723e2d2ca08e12f8a4cb3560a51c9
emifau <emiliafau.dev@gmail.com>
2024-08-30 10:18:58 +0200

    page head et blog modifié

---
+++ blog.php
@@ -0,0 +1,24 @@
+<?php require "include/head.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page blog</title>
+</head>
+
+<body>
+
+ <main>
+
+ <div class="banner">
+    <img src="./public/css/images/bannière.png" alt="bannière téléthon">
+ </div> 
+
+ </main>
+    
+
+</body>
+</html>
+
+<?php require "include/tail.php"; ?>
