b5de397fac298684ca457a6e35800471721ff711
emifau <emiliafau.dev@gmail.com>
2024-08-30 10:46:55 +0200

    modification blog

--- blog.php
+++ blog.php
@@ -1,15 +1,8 @@
 <?php require "include/head.php"; ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page blog</title>
-</head>
 
-<body>
 
- <main>
+
+<main>
 
  <div class="banner">
     <img src="./public/css/images/bannière.png" alt="bannière téléthon">
@@ -17,8 +10,5 @@
 
  </main>
     
-
-</body>
-</html>
 
 <?php require "include/tail.php"; ?>
