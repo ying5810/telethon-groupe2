ae8a17fc57226a223ba2b897507dee05022a1b6c
emifau <emiliafau.dev@gmail.com>
2024-08-30 12:00:38 +0200

    modif blog

--- blog.php
+++ blog.php
@@ -1,11 +1,58 @@
 <?php require "include/head.php"; ?>
 
-<main>
+<main id="main_blog" class="wrapper_blog">
 
  <div class="banner">
-    <img src="./public/css/images/bannière.png" alt="bannière téléthon">
- </div> 
+  <h1><img src="./public/css/images/bannière.png" alt="bannière téléthon">
+  </h1>
+   <h2><div class="title_actualités">Nos actualités</div></h2>   
+ </div>
 
- </main>
+ <div class="cartesblog">
+    
+    <section class="carteblog1">
+        <h3 class="carteblog1_titre">Les avancées de recherches</h3>
+        <div class="carteblog1_date">03/07/2024</div>
+        <blockquote class="carteblog1_description">Portail documentaire dédié aux maladies neuromusculaires  et au handicap moteur.
+        Edité par le service documentation de  l'AFM-Téléthon.......</blockquote>
+        <div class="carteblog1_cta">
+            <a href="" class="carteblog1_btn">voir article</a>
+        </div>
+   </section>
+
+   <section class="carteblog2">
+        <h3 class="carteblog2_titre">Les cahiers de Myologie</h3>
+        <div class="carteblog2_date">06/02/2024</div>
+        <blockquote class="carteblog2_description">Revue francophone de référence en myologie. 
+        les Cahiers de Myologie sont  destinés aux professionnels médicaux, paramédicaux 
+        et scientifiques impliqués dans le domaine du muscle et des maladies neuromusculaires.</blockquote>
+        <div class="carteblog2_cta">
+            <a href="" class="carteblog2_btn">voir article</a>
+        </div>
+   </section>
+
+   <section class="carteblog3">
+        <h3 class="carteblog3_titre">Myobase</h3>
+        <div class="carteblog3_date">17/06/2022</div>
+        <blockquote class="carteblog3_description">Portail documentaire dédié aux maladies neuromusculaires e
+            t au handicap moteur.
+        Edité par le service documentation de  l'AFM-Téléthon.......</blockquote>
+        <div class="carteblog3_cta">
+            <a href="" class="carteblog3_btn">voir article</a>
+        </div>
+   </section>
+
+   <section class="carteblog4">
+        <h3 class="carteblog4_titre">Les essais en France et dans le monde</h3>
+        <div class="carteblog4_date">20/06/2022</div>
+        <blockquote class="carteblog4_description">Tour d'horizon des essais cliniques en cours ou 
+        en préparation en France et dans le monde.</blockquote>
+        <div class="carteblog4_cta">
+            <a href="" class="carteblog4_btn">voir article</a>
+        </div>
+   </section>
+
+
+</main>
     
 <?php require "include/tail.php"; ?>
