f3da967b494f7574657d9ae871b13c754c0d4475
emifau <emiliafau.dev@gmail.com>
2024-09-02 08:20:45 +0200

    modif page blog

--- blog.php
+++ blog.php
@@ -12,19 +12,21 @@
     <section class="carteblog1">
         <h3 class="carteblog1_titre">Les avancées de recherches</h3>
         <div class="carteblog1_date">03/07/2024</div>
-        <blockquote class="carteblog1_description">Portail documentaire dédié aux maladies neuromusculaires  et au handicap moteur.
-        Edité par le service documentation de  l'AFM-Téléthon.......</blockquote>
+        <blockquote class="carteblog1_description">Des documents, mis à jour chaque année, 
+            pour tout savoir sur les dernières avancées de la recherche dans une maladie donnée ou un groupe de maladies.
+        </blockquote>
         <div class="carteblog1_cta">
             <a href="" class="carteblog1_btn">voir article</a>
         </div>
    </section>
 
    <section class="carteblog2">
-        <h3 class="carteblog2_titre">Les cahiers de Myologie</h3>
-        <div class="carteblog2_date">06/02/2024</div>
-        <blockquote class="carteblog2_description">Revue francophone de référence en myologie. 
-        les Cahiers de Myologie sont  destinés aux professionnels médicaux, paramédicaux 
-        et scientifiques impliqués dans le domaine du muscle et des maladies neuromusculaires.</blockquote>
+     <h3 class="carteblog2_titre">Les cahiers de Myologie</h3>
+      <div class="carteblog2_date">06/02/2024</div>
+       <blockquote class="carteblog2_description">Revue francophone de référence en myologie. 
+         les Cahiers de Myologie sont  destinés aux professionnels médicaux, paramédicaux 
+         et scientifiques impliqués dans le domaine du muscle et des maladies neuromusculaires.
+        </blockquote>
         <div class="carteblog2_cta">
             <a href="" class="carteblog2_btn">voir article</a>
         </div>
@@ -33,9 +35,9 @@
    <section class="carteblog3">
         <h3 class="carteblog3_titre">Myobase</h3>
         <div class="carteblog3_date">17/06/2022</div>
-        <blockquote class="carteblog3_description">Portail documentaire dédié aux maladies neuromusculaires e
-            t au handicap moteur.
-        Edité par le service documentation de  l'AFM-Téléthon.......</blockquote>
+        <blockquote class="carteblog3_description">Myobase est un portail documentaire dédié aux maladies neuromusculaires 
+        et au handicap moteur, édité par le service documentation de l'AFM-Téléthon. Il donne accès à plus de 60 000 documents.
+        </blockquote>
         <div class="carteblog3_cta">
             <a href="" class="carteblog3_btn">voir article</a>
         </div>
@@ -51,7 +53,27 @@
         </div>
    </section>
 
+   <section class="carteblog5">
+        <h3 class="carteblog5_titre">Les ressources pédagogiques</h3>
+        <div class="carteblog5_date">14/12/2022</div>
+        <blockquote class="carteblog5_description">Retrouvez dans cette page toutes les ressources pédagogiques mises à 
+         votre disposition par l'AFM-Téléthon, notamment pour préparer les  interventions de 1000 chercheurs dans les écoles.</blockquote>
+        <div class="carteblog5_cta">
+            <a href="" class="carteblog5_btn">voir article</a>
+        </div>
+   </section>
 
+   <section class="carteblog6">
+        <h3 class="carteblog6_titre">Le magazine VLM</h3>
+        <div class="carteblog6_date">14/12/2022</div>
+      <blockquote class="carteblog6_description">VLM est le magazine d'information de l'AFM-Téléthon. 
+        Retrouver tous les trimestres l'actualité des maladies neuromusculaires dans tous les  domaines....
+      </blockquote>
+        <div class="carteblog6_cta">
+            <a href="" class="carteblog6_btn">voir article</a>
+        </div>
+   </section>
+ </div>
 </main>
     
 <?php require "include/tail.php"; ?>
